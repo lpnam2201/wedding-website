a0a0221a5a327b685e91ad1e9400a4f49c22e491
lpnam2201 <lpnam2201@gmail.com>
2024-06-13 12:13:37 +0700

    123

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyd9Kjw6lJU2wF_qcd6fj3srjB_tfPGgcPqHhtXnbJeJ5xoJ8yO5VB3Or6bK_6WQz4u/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbz0nwJWETMFWkjH4vC-a2iy2siL8xHdBJf5ydsRbWalRX4saQr0eNazE969BeM4M5_D6A/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
